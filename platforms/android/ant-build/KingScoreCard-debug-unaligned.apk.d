7b7d4d63a164f42d59f01d479c39db89a7305423
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-05-04 18:17:55 -0400

    Removed tracking

--- platforms/android/ant-build/KingScoreCard-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/shawnhoffman/Development/King-Score-Pad/platforms/android/ant-build/KingScoreCard-debug-unaligned.apk : \
-/Users/shawnhoffman/Development/King-Score-Pad/platforms/android/ant-build/KingScoreCard.ap_ \
-/Users/shawnhoffman/Development/King-Score-Pad/platforms/android/ant-build/classes.dex \
-/Users/shawnhoffman/Development/King-Score-Pad/platforms/android/CordovaLib/ant-build/classes.jar \
